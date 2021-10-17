67d2108233304f33938b10f64b911f826071d00b
Andrew Cummings <acummings@egnyte.com>
2021-10-16 19:38:55 -0700

    dependencies

---
+++ src/test/java/com/ucla/jam/JamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucla.jam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
