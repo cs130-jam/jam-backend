7820c2fbe303180b3929b09325b58d3f39e9e490
Andrew Cummings <acummings@egnyte.com>
2021-10-16 20:08:49 -0700

    token resolver, allowed origin

--- src/main/java/com/ucla/jam/JamContext.java
+++ src/main/java/com/ucla/jam/JamContext.java
@@ -3,14 +3,42 @@ package com.ucla.jam;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ucla.jam.session.SessionTokenResolver;
 import com.ucla.jam.util.ObjectMapperProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Clock;
+import java.util.List;
 
 @Import({
         SessionTokenResolver.class
 })
 public class JamContext {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(
+            SessionTokenResolver tokenResolver,
+            @Value("${server.allowed.origin}") String allowedOrigin
+    ) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+                resolvers.add(tokenResolver);
+            }
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowedOrigin);
+            }
+        };
+    }
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
 
     @Primary
     @Bean
