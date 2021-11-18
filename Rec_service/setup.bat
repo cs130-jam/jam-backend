d7d130ac9561c7bab0e67e709981478c525f17a9
Andrew Cummings <agcummings@gmail.com>
2021-11-17 22:49:38 -0800

    exit if fail to activate virtual env

--- Rec_service/setup.bat
+++ Rec_service/setup.bat
@@ -6,7 +6,7 @@ fltmc >nul 2>&1 || (
 cd Rec_service
 
 py -3.8 -m venv venv
-call venv\Scripts\activate.bat
+call venv\Scripts\activate.bat || exit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python create_table.py
